d877529561b80351e663d82998562e86bf9b6ab5
Robert Laws <roblaws@me.com>
2019-11-27 09:45:50 +0300

    add 404 page and switch from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import MainNav from './navigation/MainNav';
 
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage';
 import About from './pages/About';
 import Articles from './pages/Articles';
 import Article from './pages/Article';
+import NotFound from './pages/NotFound';
 
 import { Layout } from 'antd/es';
 import { Row, Col } from 'antd/es';
@@ -25,10 +26,13 @@ function App() {
         <Content style={{ padding: '40px 50px' }}>
           <Row>
             <Col span={12}>
-              <Route exact path='/' component={HomePage} />
-              <Route path='/about' component={About} />
-              <Route path='/articles' component={Articles} />
-              <Route path='/article/:name' component={Article} />
+              <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/about' component={About} />
+                <Route path='/articles' component={Articles} />
+                <Route path='/article/:name' component={Article} />
+                <Route component={NotFound} />
+              </Switch>
             </Col>
           </Row>
         </Content>
--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import articles from '../data/articles';
 
 import ArticlesList from '../components/ArticlesList';
+import NotFound from '../pages/NotFound';
 
 const Article = ({ match }) => {
   const name = match.params.name;
   const article = articles.find(article => article.name === name);
 
-  if (!article) return <h1>No Article Found...</h1>;
+  if (!article) return <NotFound />;
 
   const otherArticles = articles.filter(article => article.name !== name);
 
